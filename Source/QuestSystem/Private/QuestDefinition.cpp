51c204dc9c5dbd6cfa66f70b886f556f6f5daf68
Aleksei Stepanov <73959167+AlexHeiden@users.noreply.github.com>
2026-08-13 17:56:52 +0200

    Add a data structure for quests
    Create test quest asset

---
+++ Source/QuestSystem/Private/QuestDefinition.cpp
@@ -0,0 +1,8 @@
+﻿#include "Public/Data Classes/QuestDefinition.h"
+
+const FPrimaryAssetType UQuestDefinition::QuestAssetType(TEXT("Quest"));
+
+FPrimaryAssetId UQuestDefinition::GetPrimaryAssetId() const
+{
+	return FPrimaryAssetId(QuestAssetType, QuestID.GetTagName());
+}
---
+++ Source/QuestSystem/Public/Data Classes/QuestDefinition.h
@@ -0,0 +1,33 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "GameplayTagContainer.h"
+#include "QuestObjectiveData.h"
+#include "QuestDefinition.generated.h"
+
+// Quest data asset
+UCLASS(BlueprintType)
+class UQuestDefinition: public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+public:
+	static const FPrimaryAssetType QuestAssetType;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	FText QuestName;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	FText QuestDescription;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	FGameplayTag QuestID;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	FGameplayTagContainer RequiredCompletedQuests;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TArray<FQuestObjectiveData> Objectives;
+	
+	virtual FPrimaryAssetId GetPrimaryAssetId() const override;
+};
