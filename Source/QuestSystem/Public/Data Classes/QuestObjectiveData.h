5bf5958a995a58d19eb13b5ce98c9a595b3a2a1b
Aleksei Stepanov <73959167+AlexHeiden@users.noreply.github.com>
2026-08-13 16:46:45 +0200

    Add data structure for quest objectives

---
+++ Source/QuestSystem/Public/Data Classes/QuestObjectiveData.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "QuestObjectiveData.generated.h"
+
+// Container with data on how to complete a quest's objective
+USTRUCT(BlueprintType)
+struct FQuestObjectiveData
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	FGameplayTag TriggerTag;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	FGameplayTagContainer RequiredModifierTags;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (ClampMin = "1"))
+	int32 RequiredCount = 1;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	FText Description;
+};
