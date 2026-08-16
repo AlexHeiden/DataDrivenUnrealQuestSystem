48c298429496bd534a7e49d22f27b10499c59e33
Aleksei Stepanov <73959167+AlexHeiden@users.noreply.github.com>
2026-08-16 14:05:03 +0200

    Create first version of Quest Manager Subsystem

---
+++ Source/QuestSystem/Private/QuestManagerSubsystem.cpp
@@ -0,0 +1,16 @@
+﻿#include "Public/Quest Managers/QuestManagerSubsystem.h"
+
+void UQuestManagerSubsystem::Initialize(FSubsystemCollectionBase& Collection)
+{
+	Super::Initialize(Collection);
+}
+
+void UQuestManagerSubsystem::Deinitialize()
+{
+	Super::Deinitialize();
+}
+
+int UQuestManagerSubsystem::Test()
+{
+	return CompletedQuestIDs.Num();
+}
---
+++ Source/QuestSystem/Public/Quest Managers/QuestManagerSubsystem.h
@@ -0,0 +1,45 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "GameplayTagContainer.h"
+#include "Data Classes/QuestDefinition.h"
+#include "Data Classes/QuestObjectiveData.h"
+#include "Data Classes/QuestData.h"
+#include "QuestManagerSubsystem.generated.h"
+
+USTRUCT(BlueprintType)
+struct FActiveQuestState
+{
+	GENERATED_BODY()
+
+	UPROPERTY(BlueprintReadOnly)
+	TObjectPtr<UQuestDefinition> QuestDefinition = nullptr;
+
+	UPROPERTY(BlueprintReadOnly)
+	EQuestState QuestState = EQuestState::Inactive;
+
+	UPROPERTY(BlueprintReadOnly)
+	TArray<FQuestObjectiveProgress> ObjectiveProgresses;
+};
+
+UCLASS(Blueprintable)
+class UQuestManagerSubsystem: public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+public:
+	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+	virtual void Deinitialize() override;
+
+	UFUNCTION(BlueprintCallable, Category = "Quest")
+	int Test();
+
+private:
+
+	UPROPERTY()
+	
+	
+	UPROPERTY()
+	TArray<FGameplayTag> CompletedQuestIDs;
+};
+
